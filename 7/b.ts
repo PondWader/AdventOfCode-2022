ae447a1615e379511fef33bcc6d3fae5dcbf1e78
PondWader <66561610+PondWader@users.noreply.github.com>
2022-12-07 16:45:38 +0000

    Day 7

---
+++ 7/b.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+import path from "path";
+
+const input = fs.readFileSync('./7/input.txt', 'utf-8').replaceAll('\r', '');
+const lines = input.split('\n');
+
+const dirs: {[key: string]: number} = {}
+
+let currentDir = '';
+while (lines[0]) {
+    const [_, command, arg] = lines.shift()?.split(' ') as [string, string, string];
+
+    switch (command) {
+        case "cd":
+            currentDir = path.join(currentDir, arg).replaceAll('\\', '/');;
+            break;
+
+        case "ls":
+            let dirSize = 0;
+            while (lines[0] && !lines[0].startsWith('$')) {
+                if (lines[0].startsWith('dir ')) {
+                    lines.shift();
+                    continue;
+                }
+
+                const [fileSize] = lines.shift()!.split(' ').map(a => parseInt(a) || a) as [number, string];
+                dirSize += fileSize;
+            }
+            dirs[currentDir] = dirSize;
+            
+            let backPath = currentDir;
+            while (backPath !== '/') {
+                backPath = path.join(backPath, '..').replaceAll('\\', '/');
+                dirs[backPath] += dirSize;
+            }
+            break;
+    }
+}
+
+const requiredToFinish = 30000000- (70000000 - dirs["/"])
+console.log(
+  Object.values(dirs).sort((a, b) => a - b)
+  .find(v => v >= requiredToFinish)
+)
