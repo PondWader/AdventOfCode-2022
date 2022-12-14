4b3d0fe6aa906c4c271178952012b89075ba7ddc
PondWader <66561610+PondWader@users.noreply.github.com>
2022-12-14 17:06:42 +0000

    Day 14

---
+++ 14/a.ts
@@ -0,0 +1,53 @@
+import fs from "fs";
+
+const input = fs.readFileSync('./14/input.txt', 'utf-8').replaceAll('\r', '');
+
+const map = new Map<string, 'sand' | 'air' | 'rock'>();
+
+let lowestY = 0;
+for (const path of input.split('\n')) {
+    const points = path.split(' -> ').map(p => p.split(',').map(c => parseInt(c)));
+
+    points.forEach((point, i) => {
+        const [x, y] = point;
+        if (!points[i + 1]) return;
+        const [nextX, nextY] = points[i + 1];
+
+        map.set(x + ',' + y, 'rock')
+        for (let xChange = nextX - x; xChange !== 0; xChange > 0 ? xChange-- : xChange++) {
+            map.set(`${x + xChange},${y}`, 'rock');
+        }
+        for (let yChange = y - nextY; yChange !== 0; yChange > 0 ? yChange-- : yChange++) {
+            map.set(`${x},${y - yChange}`, 'rock');
+            if (y - yChange > lowestY) lowestY = y - yChange;
+        }
+    })
+}
+
+let restingSand = 0;
+function dropSand(x: number, y: number): boolean {
+    if (y > lowestY) return false;
+
+    let res = false;
+    if (![
+        [x, y + 1],
+        [x - 1, y + 1],
+        [x + 1, y + 1]
+    ].some(([newX, newY]) => {
+        if (map.get(`${newX},${newY}`) === undefined) {
+            res = dropSand(newX, newY);
+            return true
+        }
+    })) {
+        map.set(`${x},${y}`, 'sand');
+        restingSand++;
+        res = true;
+    }
+
+    return res;
+}
+
+while (true) {
+    if (!dropSand(500, 0)) break;
+}
+console.log(restingSand)
